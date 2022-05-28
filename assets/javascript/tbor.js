3fa9eced3ff438ecd8f2452a80c664a2f6e41ff3
0x00 <85400943+tbor8080@users.noreply.github.com>
2022-05-28 17:54:15 +0900

    gh pages (#12)
    * js update
    
    * js update02
    
    * js update02
    
    * js update
    
    Co-authored-by: ryohei.suga@gmail.com <ryohei@kanryouhei-no-MacBook-Air.local>

--- assets/javascript/tbor.js
+++ assets/javascript/tbor.js
@@ -60,7 +60,11 @@ function Login(){
     url = "/"+digest+"/";
     if(navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/i)){
         // location.replace("./404.html");
+<<<<<<< HEAD
+        // alert("PCでご覧ください。");
+=======
         alert("PCでご覧ください。");
+>>>>>>> origin/main
         $("body").html("");
     }else{
         location.replace(url);
@@ -79,7 +83,7 @@ $(function(){
             var image = new Image();
             str = j;
             if( j < 10 ) str = "0"+j;
-            image.src = preload_image_dir + preload_image_content + '/screen000' + str;
+            image.src = preload_image_dir + preload_image_content[i] + '/screen000' + str + '.png';
         }
     }
     var body = document.body;
@@ -146,6 +150,8 @@ $(function(){
         //PC
         $("body").css("overflow","hidden");
     }
+<<<<<<< HEAD
+=======
 
 <<<<<<< HEAD
     
@@ -156,6 +162,7 @@ $(function(){
         $("body").html("");
     }
 >>>>>>> c566e0da003f8ec702956610867ba89c198d49e2
+>>>>>>> origin/main
     function SlideShow(){
         // slide show
         counter = 0;
